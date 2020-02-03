bd52e30d82153f4058d6112aaf33620510391d4a
Pim Snel <pim@lingewoud.nl>
2020-02-03 01:25:03 +0100

    try my own

--- sc-im.rb
+++ sc-im.rb
@@ -3,15 +3,15 @@ class ScIm < Formula
   homepage "https://github.com/andmarti1424/sc-im"
   url "https://github.com/andmarti1424/sc-im/archive/v0.6.0.tar.gz"
   sha256 "5da644d380ab3752de283b83cce18c3ba12b068d0762c44193c34367a0dcbc38"
-  head "https://github.com/andmarti1424/sc-im.git", :branch => "freeze"
+  head "https://github.com/mipmip/sc-im.git", :branch => "mdexport"
   revision 3
 
-  bottle do
-    root_url "https://dl.bintray.com/nickolasburr/homebrew-bottles"
+  #bottle do
+  #  root_url "https://dl.bintray.com/nickolasburr/homebrew-bottles"
 
-    sha256 "28c429962a95214a9e94b98a9fc48cd276c67fd9e90958a38346aea7b963693b" => :high_sierra
-    sha256 "28c429962a95214a9e94b98a9fc48cd276c67fd9e90958a38346aea7b963693b" => :sierra
-  end
+  #  sha256 "28c429962a95214a9e94b98a9fc48cd276c67fd9e90958a38346aea7b963693b" => :high_sierra
+  #  sha256 "28c429962a95214a9e94b98a9fc48cd276c67fd9e90958a38346aea7b963693b" => :sierra
+  #end
 
   patch :DATA
 
@@ -43,7 +43,7 @@ index 779f8bb..3443037 100644
  ifeq ($(shell uname -s),Darwin)
    NCURSES_CFLAGS ?=
 -  NCURSES_LIBS   ?= -lncursesw
-+  NCURSES_LIBS   ?= -lncurses
++  NCURSES_LIBS   ?= -/
  else ifeq ($(shell pkg-config --exists ncursesw || echo 'no'),no)
    NCURSES_CFLAGS ?=
    NCURSES_LIBS   ?= -lncursesw
